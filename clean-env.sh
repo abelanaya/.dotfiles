f068d48b118ed5a2aab4d6df17f023d2e320ce0c
AbelAnaya <abelanaya97@gmail.com>
2025-05-21 19:40:05 +0200

    fix(stow): Source stow config from zsh_profile

--- clean-env.sh
+++ clean-env.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
+
+source zsh/.zsh_profile
+
 # I am using zsh instead of bash.  I was having some troubles using bash with
 # arrays.  Didn't want to investigate, so I just did zsh
 pushd $DOTFILES
