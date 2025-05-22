40d0bba6115dad3521f6a4c0ac97fea13eb71616
AbelAnaya <abelanaya97@gmail.com>
2025-05-23 00:14:44 +0200

    feat(tools): Enable nvim transparency

--- nvim/.config/nvim/plugin/set.lua
+++ nvim/.config/nvim/plugin/set.lua
@@ -144,5 +144,5 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- Set color scheme
 vim.cmd("colorscheme onedark_vivid")
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
